57ced358cd8fd672059136e7c2a0cc01238b50bd
dzik1979 <janowski_andrzej@wp.pl>
2013-04-19 14:38:44 +0100

    ?

---
+++ lib/Frontend.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Consult documentation on http://agiletoolkit.org/learn 
+ */
+class Frontend extends ApiFrontend {
+    function init(){
+        parent::init();
+        // Keep this if you are going to use database on all pages
+        //$this->dbConnect();
+        $this->requires('atk','4.2.0');
+        $this->add('jUI');
+
+
+    }
+
+
+}
